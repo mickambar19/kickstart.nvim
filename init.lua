97d7501e60f8bece4f4992bc939a41d9bc132152
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-01-23 21:01:16 -0600

    feat: use prettierd in typescriptreact

--- init.lua
+++ init.lua
@@ -772,6 +772,7 @@ require('lazy').setup({
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
