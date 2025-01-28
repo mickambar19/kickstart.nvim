4268977154e910a7d6b23dc631ce1839601f765d
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2025-01-27 23:38:15 -0600

    feat: close windows keymap

--- init.lua
+++ init.lua
@@ -188,6 +188,9 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>x', '<cmd>close<CR>', { desc = 'Close window' })
+vim.keymap.set('n', '<leader>X', '<cmd>qa<CR>', { desc = 'Close all windows' })
+vim.keymap.set('n', '<C-w>x', '<C-w>c', { desc = 'Close window keep buffer' })
 
 -- Speed improvements neovim
 --
